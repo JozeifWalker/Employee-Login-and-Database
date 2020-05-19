4c61ed08671769b957d8439ef9f647a09f74acb0
Jozeif Walker <jozeif.walker@ibm.com>
2020-05-19 16:10:04 -0500

    Changes to keys.js, readme, and deleted .gitattributes

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mongoURI: "mongodb+srv://jozeif:thisismypassword@cluster0-bd8qa.mongodb.net/test?retryWrites=true&w=majority",
+    mongoURI: "mongodb://jozeif:thisismypassword@cluster0-shard-00-00-bd8qa.mongodb.net:27017,cluster0-shard-00-01-bd8qa.mongodb.net:27017,cluster0-shard-00-02-bd8qa.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority",
     secretOrKey: "secret"
 };
